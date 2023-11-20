cd1e0d235b13290508076ab7767903e376b00518
Enrique Martinez <enrique.martinez@iespablopicasso.es>
2023-11-20 08:34:39 +0100

    Inicio proyecto

---
+++ JavaDebugger/src/JavaDebugger/JavaDebugger.java
@@ -0,0 +1,12 @@
+package JavaDebugger;
+
+public class JavaDebugger {
+
+	public static void main(String[] args) {
+		ClaseParaDepurar clase = new ClaseParaDepurar();
+        double valor = clase.echaCuentas();
+        System.out.println("Sale "+valor);
+
+	}
+
+}
